a28e0697cfddd7a4f8578a5666836fdd75050969
Taehwan Kim <freewebwithme777@protonmail.com>
2020-07-02 16:03:20 -0700

    change server address dynamically

--- src/api-config.js
+++ src/api-config.js
@@ -3,7 +3,7 @@ let serverWSHost;
 
 const hostname = window && window.location && window.location.hostname;
 
-if (hostname === "churchapp.dev") {
+if (hostname === "churchapp.dev" || hostname === "www.churchapp.dev") {
   serverHTTPHost = "https://churchapp-server.herokuapp.com/api";
   serverWSHost = "ws://churchapp-server.herokuapp.com/socket";
 } else {
